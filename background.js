7e89c1e676730a051fa4ace9fd5f12a53de96ccf
David Chen <davidc3287@gmail.com>
2021-08-11 15:51:10 -0400

    refactor logic

--- background.js
+++ background.js
@@ -1,6 +1,5 @@
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
   if (msg.action === "updateIcon") {
-    console.log("action received");
     chrome.action.setIcon({ path: "icons/login128.png" });
     // change back to default icon after a delay
     setTimeout(() => {
