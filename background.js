128c80563aa109e795cd3186aa127520af75d224
David Chen <davidc3287@gmail.com>
2021-08-11 15:32:42 -0400

    icon changes

--- background.js
+++ background.js
@@ -1,10 +1,10 @@
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
   if (msg.action === "updateIcon") {
     console.log("action received");
-    chrome.browserAction.setIcon({ path: "icons/login.png" });
+    chrome.browserAction.setIcon({ path: "icons/login128.png" });
     // change back to default icon after a delay
     setTimeout(() => {
-      chrome.browserAction.setIcon({ path: "icons/icon.png" });
+      chrome.browserAction.setIcon({ path: "icons/icon128.png" });
     }, 5000);
   }
 });
