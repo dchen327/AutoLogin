441de1e8fb571bd89b73ddb97522f29c99638552
David Chen <davidc3287@gmail.com>
2021-08-11 10:24:07 -0400

    add sites

--- content.js
+++ content.js
@@ -1,6 +1,19 @@
-// const loginButton1 = document.querySelector("#siteNavBar_loginToggle");
-// const loginButton2 = document.querySelector("#siteNavBar_ctl00_btnLogin");
+// const loginButton1 = document.querySelector("#siteNavBar_loginToggle");  // first login
+// const loginButton2 = document.querySelector("#siteNavBar_ctl00_btnLogin"); // second log-in (shows beneath first)
 // if (loginButton1) loginButton1.click();
 // if (loginButton2) loginButton2.click();
 
-console.log("URL:", location.href);
+const signIn = (url) => {
+  if (url in sites) {
+    console.log("IN SITES:", url);
+    const elementsToClick = document.querySelectorAll(sites[url]);
+    console.log(elementsToClick);
+    elementsToClick.forEach((element) => {
+      element.click();
+    });
+  }
+};
+
+signIn(location.origin);
+
+console.log("URL:", location.origin);
