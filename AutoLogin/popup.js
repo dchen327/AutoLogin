fef7883b1ad4769b2a4850d14b36ab1b0731abf3
David Chen <davidc3287@gmail.com>
2021-08-13 09:25:48 -0400

    better organized functions

--- AutoLogin/popup.js
+++ AutoLogin/popup.js
@@ -1,12 +1,26 @@
 const selectorInput = document.getElementById("selectorInput");
 const addNewSelector = document.getElementById("addNewSelector");
 
+// grab the base URL (to use as key in storage)
+const getTabURL = async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  return new URL(tab.url).origin;
+};
+
+const getSelectors = async () => {
+  let url = await getTabURL();
+  chrome.storage.sync.get({ [url]: [] }, (res) => {
+    let selectors = res[url];
+    alert(selectors);
+  });
+};
+
 addNewSelector.addEventListener("click", async (e) => {
   e.preventDefault();
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  alert(new URL(tab.url).origin);
+  let url = await getTabURL();
   let selector = selectorInput.value;
-  // let url = location.origin;
-  // chrome.storage.sync.set()
+  // chrome.storage.sync.set({[url]: [selector]})
   selectorInput.value = "";
 });
+
+getSelectors();
