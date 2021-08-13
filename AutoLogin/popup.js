7f9c7975d8b954203c7d06ef3da06a4fd951c996
David Chen <davidc3287@gmail.com>
2021-08-13 11:04:04 -0400

    delete selectors

--- AutoLogin/popup.js
+++ AutoLogin/popup.js
@@ -21,17 +21,31 @@ const setupPopup = async () => {
   const selectorList = document.getElementById("selectorList");
   console.log(selectors);
   // show "None" in list of elements
-  if (selectors.length === 0) selectors.push("None");
+  // if (selectors.length === 0) selectors.push("None");
   // render elements in list
   selectors.forEach((selector) => {
-    let li = `
-    <li id=${selector}>${selector}</li>
+    let tr = `
+    <tr>
+      <td>${selector}</td>
+      <td><a id=${selector}>[x]</a></td>
+    </tr>
     `;
-    selectorList.insertAdjacentHTML("beforeend", li);
+    selectorList.insertAdjacentHTML("beforeend", tr);
+    // attach delete button to delete function
+    document.getElementById(selector).addEventListener("click", deleteSelector);
   });
 };
 
-const deleteSelector = async () => {};
+const deleteSelector = async (e) => {
+  e.preventDefault();
+  let url = await getTabURL();
+  const selector = e.target.id; // grab id from clicked element
+  let selectors = await getSelectors();
+  const index = selectors.indexOf(selector);
+  if (index > -1) selectors.splice(index, 1);
+  await chrome.storage.sync.set({ [url]: selectors });
+  window.close();
+};
 
 addNewSelector.addEventListener("click", async (e) => {
   e.preventDefault();
