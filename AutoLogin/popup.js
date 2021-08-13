f6bdb72c82ab58ac93012687ac05089c7f2ec95f
David Chen <davidc3287@gmail.com>
2021-08-13 09:10:52 -0400

    get origin url

--- AutoLogin/popup.js
+++ AutoLogin/popup.js
@@ -3,6 +3,10 @@ const addNewSelector = document.getElementById("addNewSelector");
 
 addNewSelector.addEventListener("click", async (e) => {
   e.preventDefault();
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  alert(new URL(tab.url).origin);
   let selector = selectorInput.value;
+  // let url = location.origin;
+  // chrome.storage.sync.set()
   selectorInput.value = "";
 });
