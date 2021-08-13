9b67f03806bd9566c50ce83a76b3bdf72cae471d
David Chen <davidc3287@gmail.com>
2021-08-13 09:04:13 -0400

    clean up

--- AutoLogin/popup.js
+++ AutoLogin/popup.js
@@ -1,6 +1,6 @@
 const selectorInput = document.getElementById("selectorInput");
 const addNewSelector = document.getElementById("addNewSelector");
-console.log(addNewSelector);
+
 addNewSelector.addEventListener("click", async (e) => {
   e.preventDefault();
   let selector = selectorInput.value;
