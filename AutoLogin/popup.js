f681513d79894d29d835351c0cdbd3a01b5bcc50
David Chen <davidc3287@gmail.com>
2021-08-13 11:31:31 -0400

    better delete

--- AutoLogin/popup.js
+++ AutoLogin/popup.js
@@ -16,35 +16,38 @@ const getSelectors = async () => {
   });
 };
 
-const setupPopup = async () => {
-  let selectors = await getSelectors();
+const renderPopup = async () => {
   const selectorList = document.getElementById("selectorList");
-  console.log(selectors);
-  // show "None" in list of elements
-  // if (selectors.length === 0) selectors.push("None");
+  let selectors = await getSelectors();
+  let newListHTML = "";
   // render elements in list
-  selectors.forEach((selector) => {
+  selectors.forEach((selector, index) => {
     let tr = `
     <tr>
       <td>${selector}</td>
-      <td><a id=${selector}>[x]</a></td>
+      <td><a id=selector${index} row=${index}>[x]</a></td>
     </tr>
     `;
-    selectorList.insertAdjacentHTML("beforeend", tr);
-    // attach delete button to delete function
-    document.getElementById(selector).addEventListener("click", deleteSelector);
+    // selectorList.insertAdjacentHTML("beforeend", tr);
+    newListHTML += tr;
   });
+  selectorList.innerHTML = newListHTML; // update HTML
+  // attach delete buttons to delete function
+  for (let index = 0; index < selectors.length; index++) {
+    document
+      .getElementById(`selector${index}`)
+      ?.addEventListener("click", deleteSelector);
+  }
 };
 
 const deleteSelector = async (e) => {
   e.preventDefault();
+  let index = e.target.attributes.row.value;
   let url = await getTabURL();
-  const selector = e.target.id; // grab id from clicked element
   let selectors = await getSelectors();
-  const index = selectors.indexOf(selector);
-  if (index > -1) selectors.splice(index, 1);
+  selectors.splice(index, 1);
   await chrome.storage.sync.set({ [url]: selectors });
-  window.close();
+  await renderPopup();
 };
 
 addNewSelector.addEventListener("click", async (e) => {
@@ -55,7 +58,7 @@ addNewSelector.addEventListener("click", async (e) => {
   let selectors = await getSelectors();
   selectors.push(newSelector);
   await chrome.storage.sync.set({ [url]: selectors });
-  window.close();
+  await renderPopup();
 });
 
-setupPopup();
+renderPopup();
