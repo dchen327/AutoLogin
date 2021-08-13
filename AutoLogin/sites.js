50bc7e5b32083492abbc34683715dd13b6d120d5
David Chen <davidc3287@gmail.com>
2021-08-13 12:56:16 -0400

    remove old code

--- AutoLogin/sites.js
+++
@@ -1,8 +0,0 @@
-const sites = {
-  "https://identity.hmc.edu": ["#authn-go-button"],
-  "https://webauth.claremont.edu": [
-    "#list-providers > ul > li:nth-child(1) > a",
-  ],
-  "https://portal.hmc.edu": ["#siteNavBar_ctl00_btnLogin"],
-  "https://sakai.claremont.edu": ["#loginLink1"],
-};
