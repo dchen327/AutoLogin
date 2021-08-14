981f24a0b3c5fc5233dcea944787ca9187c66b8c
David Chen <davidc3287@gmail.com>
2021-08-14 12:59:24 -0400

    manifest

--- AutoLogin/content.js
+++ AutoLogin/content.js
@@ -3,49 +3,41 @@ const signIn = () => {
   chrome.storage.sync.get([url], (res) => {
     if (!(typeof res[url] === "undefined")) {
       let selectors = res[url];
-      let retryCount = 3;
-      let intervalID = setInterval(() => {
-        if (retryCount-- <= 0) {
-          clearInterval(intervalID);
-        }
-        console.log("attempt to signin", new Date().getSeconds());
-        // grab elements, convert to array to use .every()
-        const elementsToClick = Array.from(
-          document.querySelectorAll(selectors)
-        );
-        // all elements present
-        if (elementsToClick.length) {
-          let signInButtonFound = false;
-          elementsToClick.every((element) => {
-            // avoid clicking sign out buttons (this needs improvement but idk how)
-            const elementText = element.innerText.toLowerCase();
-            if (elementText.includes("out")) {
-              clearInterval(intervalID);
-              signInButtonFound = false; // ignore others
-              return false; // break out of loop
-            } else {
-              try {
-                // might give an error if element has inline js, so wrap in try/catch
-                element.click();
-              } catch (error) {}
-              signInButtonFound = true;
-              return true; // continue the every()
-            }
-          });
-          if (signInButtonFound) {
-            // login was clicked
-            chrome.runtime.sendMessage({
-              action: "updateIcon",
-            });
+      console.log("attempt to signin", new Date().getSeconds());
+      // grab elements, convert to array to use .every()
+      const elementsToClick = Array.from(document.querySelectorAll(selectors));
+      console.log("elements", elementsToClick);
+      // all elements present
+      if (elementsToClick.length) {
+        let signInButtonFound = false;
+        elementsToClick.every((element) => {
+          // avoid clicking sign out buttons (this needs improvement but idk how)
+          const elementText = element.innerText.toLowerCase();
+          if (elementText.includes("out")) {
+            signInButtonFound = false; // ignore others
+            return false; // break out of loop
+          } else {
+            try {
+              // might give an error if element has inline js, so wrap in try/catch
+              element.click();
+            } catch (error) {}
+            signInButtonFound = true;
+            return true; // continue the every()
           }
-        } else {
-          clearInterval(intervalID);
+        });
+        if (signInButtonFound) {
+          // login was clicked
+          chrome.runtime.sendMessage({
+            action: "updateIcon",
+          });
         }
-      }, 3000); // wait in ms before retrying
+      }
     }
   });
 };
 
-chrome.storage.sync.get({ enabled: true }, (res) => {
-  if (res.enabled) signIn();
+window.addEventListener("load", () => {
+  chrome.storage.sync.get({ enabled: true }, (res) => {
+    if (res.enabled) signIn();
+  });
 });
