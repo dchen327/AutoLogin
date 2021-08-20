b777683a6d4a4001030de0577204bca35c0281ff
David Chen <davidc3287@gmail.com>
2021-08-20 10:42:26 -0400

    display info properly on overview

--- AutoLogin/overview.js
+++ AutoLogin/overview.js
@@ -3,32 +3,24 @@ const renderSelectorList = async () => {
   chrome.storage.sync.get(null, (allData) => {
     console.log(allData);
     let row = 0;
-    for (const [url, selectors] of Object.entries(allData)) {
+    for (const [url, selectorInfo] of Object.entries(allData)) {
       // ignore toggle state data
       if (url === "enabled") continue;
-      for (let [i, selector] of selectors.entries()) {
-        if (typeof selector === "string") {
-          selectors[i] = {
-            element: selector,
-            retries: 1,
-            delay: 0,
-          };
-        }
-      }
-      chrome.storage.sync.set({ [url]: selectors });
 
-      // const websiteLabel = `<a href=${url} class="pt-1 has-text-weight-semibold" target="_blank">${url}</a>`;
-      // selectorList.insertAdjacentHTML("beforeend", websiteLabel);
-      // // render selectors in list
-      // selectors.forEach((selector) => {
-      //   let tr = `
-      //     <tr>
-      //       <td id}>${selector}</td>
-      //     </tr>
-      //   `;
-      //   selectorList.insertAdjacentHTML("beforeend", tr);
-      //   row++;
-      // });
+      const websiteLabel = `<a href=${url} class="pt-1 has-text-weight-semibold" target="_blank">${url}</a>`;
+      selectorList.insertAdjacentHTML("beforeend", websiteLabel);
+      // render selectors in list
+      selectorInfo.forEach((selector) => {
+        let tr = `
+          <tr>
+            <td id}>${selector.element}</td>
+            <td align="center">Delay: ${selector.delay}ms</td>
+            <td align="center">Retries: ${selector.retries}</td>
+          </tr>
+        `;
+        selectorList.insertAdjacentHTML("beforeend", tr);
+        row++;
+      });
     }
   });
 };
